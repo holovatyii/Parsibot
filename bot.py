cfd475f349d720226ae97f76636fb22c46fbb86d
holovatyii <85766652+holovatyii@users.noreply.github.com>
2025-04-18 21:08:39 +0100

    bot.py

--- bot.py
+++ bot.py
@@ -65,4 +65,10 @@ def webhook():
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
+    @app.route("/ip")
+def show_ip():
+    import requests
+    ip = requests.get("https://api.ipify.org").text
+    return f"Render IP: {ip}"
+
 
