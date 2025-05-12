5d4566809e0eea8768796758e9b7363cc6e0ed44
holovatyii <85766652+holovatyii@users.noreply.github.com>
2025-05-12 21:48:03 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -233,7 +233,7 @@ def webhook():
         send_telegram_message(
             f"✅ Ордер виконано. Пара: {symbol}, Сторона: {side}, TP: {tp}, SL: {actual_sl}"
         )
-print("⚙️ Викликаємо log_trade_to_csv...")
+        print("⚙️ Викликаємо log_trade_to_csv...")
 
         log_trade_to_csv({
             "timestamp": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
