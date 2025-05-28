dce7de2dc5cddde83cc665c28411a5a61c17d2e1
holovatyii <85766652+holovatyii@users.noreply.github.com>
2025-05-28 15:41:06 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -885,6 +885,7 @@ def track_open_trades():
 threading.Thread(target=track_open_trades, daemon=True).start()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=10000)
+
+
 
